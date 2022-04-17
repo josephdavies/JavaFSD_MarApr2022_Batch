727b6a3fe64ba1e661cc9e649c7c5c59e3223e37
Joseph Davies <josephdavies24@gmail.com>
2022-04-17 10:39:46 +0530

    day5

---
+++ Day5projects/project1/src/mypackage/MyClass.java
@@ -0,0 +1,14 @@
+package mypackage;
+
+public class MyClass 
+{
+	public static void main(String[] args) 
+	{
+		Employee emp = new Employee(101,"joseph",5000);
+		
+		
+		emp.printData();
+		
+
+	}
+}
