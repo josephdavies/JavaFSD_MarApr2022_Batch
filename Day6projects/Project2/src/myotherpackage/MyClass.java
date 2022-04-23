d34f27eeb319c386d4c890c9b060ac8686655e0b
Joseph Davies <josephdavies24@gmail.com>
2022-04-23 23:43:20 +0530

    inheritance and abstractclas

---
+++ Day6projects/Project2/src/myotherpackage/MyClass.java
@@ -0,0 +1,35 @@
+package myotherpackage;
+
+import mypackage.MyBaseClass;
+
+class OtherPackageDerivedClass extends MyBaseClass
+{
+	public void otherPackageDerivedClassMethod()
+	{
+		a=5;
+		b=10; //cant access private
+		c=15;
+		d=20;
+	}
+}
+
+class OtherPackageOtherClass
+{
+	public void otherPackageOtherClassMethod()
+	{
+		MyBaseClass mb = new MyBaseClass();
+		mb.a=5;
+		mb.b=5;
+		mb.c=5;
+		mb.d=5;
+	}
+}
+
+
+public class MyClass 
+{
+	public static void main(String[] args) 
+	{
+	OtherPackageDerivedClass obj = new OtherPackageDerivedClass(); 
+	}
+}
