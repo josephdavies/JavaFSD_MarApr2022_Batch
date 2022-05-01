049de409b21b59a6c5a4a62e50f0181f294eefec
Joseph Davies <josephdavies24@gmail.com>
2022-05-01 10:06:35 +0530

    read from file

---
+++ Day7projects/LearnworkingwithFiles/src/mypackage/MyClass.java
@@ -0,0 +1,28 @@
+package mypackage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MyClass 
+{
+	public static void main(String[] args) throws IOException 
+	{
+		//Step-1 : Create file object
+		File f = new File("C:\\Users\\joseph davies\\Desktop\\simplilearn\\myfiles\\scores.txt");
+		FileReader fr = new FileReader(f);
+		
+		//read the first character in ascii number
+		int i=fr.read();
+		
+		while(i!=-1)
+		{
+			System.out.print((char)i);
+			i=fr.read();
+		}
+		fr.close();
+
+	}
+}
