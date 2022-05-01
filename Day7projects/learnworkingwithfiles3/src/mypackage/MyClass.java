0e058baabd675a141c4c1b166692d556f86000af
Joseph Davies <josephdavies24@gmail.com>
2022-05-01 11:04:18 +0530

    read from file all lines

---
+++ Day7projects/learnworkingwithfiles3/src/mypackage/MyClass.java
@@ -0,0 +1,17 @@
+package mypackage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class MyClass 
+{
+	public static void main(String[] args) throws IOException 
+	{
+		var allLines = Files.readAllLines(Paths.get("C:\\Users\\joseph davies\\Desktop\\simplilearn\\myfiles\\scores.txt"));
+
+		for(var r:allLines)
+			System.out.println(r);
+		
+	}
+}
