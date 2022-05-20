0795a63a3b31632319e6bb60cc309688a0b0b5a3
Joseph Davies <josephdavies24@gmail.com>
2022-05-20 18:49:47 +0530

    counting vowels in words of string

---
+++ practise/vowels_in_the_words_of_string/src/mypackage/MyClass.java
@@ -0,0 +1,35 @@
+package mypackage;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class MyClass {
+
+	public static void main(String[] args) 
+	{
+		Scanner obj = new Scanner(System.in);
+		System.out.println("Enter a sentence");
+		String userstring = obj.nextLine();
+		
+		StringTokenizer st = new StringTokenizer(userstring," ");
+		
+		System.out.println(st);
+		System.out.println("vowel count for each word in the sentence is :");
+		
+		while(obj.hasMoreTokens())
+		{
+			String word = st.nextToken();
+			System.out.println("processing word is :"+word);
+			int vowelcount = 0;
+			for(char c:word.toCharArray())
+			{
+				c= char.toLowerCase(c);
+				if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+					vowelcount++;
+			}
+		System.out.println("the word"+word+"has vowel count"+vowelcount);
+		} 		
+
+	}
+
+}
